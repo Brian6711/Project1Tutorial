45aeae0329b402a706eef38649425e4287d9d55b
Norma Garcia <misbendiciones@10-13-218-51.vlan868.client.wireless.uic.edu>
2022-09-27 08:42:38 -0500

    done

--- sketch.js
+++ sketch.js
@@ -1,75 +1,106 @@
 
 
 class Block{
-    constructor(x,y,width,height,speedX,speedY,clr,d){
+    constructor(x,y,width,height,speedX,speedY,clr1,clr2,clr3,d){
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height;
     this.speedX = speedX;
     this.speedY = speedY;
-    this.clr = clr;
+    this.clr1 = clr1;
+    this.clr2 = clr2;
+    this.clr3 = clr3;
     this.d = d;
     }
    
 display1(){
-    fill(this.clr);
-    rect(this.x,this.y,this.width,this.height);
-    if (this.clr < 0){
-this.clr = this.clr + 255
+    if (keyIsDown(32)){
+this.clr1 = (random(0,255));
+this.clr2 = (random(0,255));
+this.clr3 = (random(0,255));
     }
+
+    else{
+    fill(this.clr1,this.clr2,this.clr3);
+    rect(this.x,this.y,this.width,this.height);
+    
+    if ((this.clr1 < 0) || (this.clr2 < 0) || (this.clr3 < 0)){
+        this.clr1 = (random(100,220));
+        this.clr2 = (random(100,220));
+        this.clr3 = (random(100,220));
+    }
+}
 }
 
 display2(){
-    fill(this.clr)
-    circle(this.x,this.y,this.d)
-    if (this.clr < 0){
-        this.clr = this.clr + 255
-            }
+    if (keyIsDown(32)){
+        this.clr1 = (random(0,255));
+        this.clr2 = (random(0,255));
+        this.clr3 = (random(0,255));
+    }
 
+    else{
+        fill(this.clr1,this.clr2,this.clr3);
+        circle(this.x,this.y,this.d)
+        if ((this.clr1 < 0) || (this.clr2 < 0) || (this.clr3 < 0)){
+            this.clr1 = (random(100,220));
+        this.clr2 = (random(100,220));
+        this.clr3 = (random(100,220));
+                }
+    
+    }
 }
 
 
 move1(){
     this.x = this.x + this.speedX
     if(this.x > (windowWidth - this.width) || this.x <= 0){
-        this.speedX = this.speedX * -1
-        this.clr = this.clr - 1
+        this.speedX = this.speedX * -1;
+        this.clr1 = this.clr1 - 1;
+        this.clr2 = this.clr2 - 1;
+        this.clr3 = this.clr3 - 1;
     }
 
     this.y = this.y + this.speedY
     if(this.y > (windowHeight - this.height) || this.y <= 0){
-        this.speedY = this.speedY * -1
-        this.clr = this.clr - 1
+        this.speedY = this.speedY * -1;
+        this.clr1 = this.clr1 - 1;
+        this.clr2 = this.clr2 - 1;
+        this.clr3 = this.clr3 - 1;
     }
 }
 
 move2(){
     this.x = this.x + this.speedX
     if(this.x > (windowWidth - this.d/2) || this.x <= 0){
-        this.speedX = this.speedX * -1
-        this.clr = this.clr - 1
+        this.speedX = this.speedX * -1;
+        this.clr1 = this.clr1 - 1;
+        this.clr2 = this.clr2 - 1;
+        this.clr3 = this.clr3 - 1;
     }
 
     this.y = this.y + this.speedY
     if(this.y > (windowHeight - this.d/2) || this.y <= 0){
-        this.speedY = this.speedY * -1
-        this.clr = this.clr - 1
+        this.speedY = this.speedY * -1;
+        this.clr1 = this.clr1 - 1;
+        this.clr2 = this.clr2 - 1;
+        this.clr3 = this.clr3 - 1;
     }
-}
 
 }
 
+}
 
 
 
 
-let Block1 = new Block (50,50,30,30,25,215,225,25);
-let Block2 = new Block (100,75,50,50,215,155,245,50);
-let Block3 = new Block (150,100,70,70,215,25,215,50);
-let Block4 = new Block (200,125,90,100,155,195,255,75);
-let Block5 = new Block (250,150,80,60,215,135,235,35);
-let Block6 = new Block (300,175,60,70,135,215,205,95);
+let Block1 = new Block (50,50,30,30,25,215,255,255,255,25);
+let Block2 = new Block (100,75,50,50,215,155,255,255,255,50);
+let Block3 = new Block (150,100,70,70,215,25,115,155,155,50);
+let Block4 = new Block (200,125,90,100,155,195,255,255,255,75);
+let Block5 = new Block (250,150,80,60,215,135,115,115,155,35);
+let Block6 = new Block (300,175,60,70,135,215,115,115,115,95);
 
 
    function setup(){
@@ -78,8 +109,15 @@ let Block6 = new Block (300,175,60,70,135,215,205,95);
 }
 
 function draw() {
-  background(0);
-  
+  if(keyIsDown(16)){
+    background(random (0,255),random(0,255),random(0,255));
+  }
+else{
+    background(0);
+}
+
+
+
 Block1.display1();
 Block1.move1();
 Block2.display1();
@@ -106,9 +144,8 @@ Block5.move2();
 Block6.display2();
 Block6.move2();
 
-
-
 }
+
 
 
 
